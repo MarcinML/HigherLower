0bdcfeafce29ecbae762e553f622373856e1823d
MarcinML <marcinliszcz@gmail.com>
2018-03-13 13:23:47 +0100

    Added input validation

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
+#include <limits>
 
 int pickRandomNumber()
 {
@@ -14,6 +15,7 @@ srand(time(NULL));
 char quit=0;
 int chances;
 int answer=0;
+bool succes{};
 
 std::cout<<"Welcome to Higher/Lower game!\nRules are simple:\n-You have 7 chances to guess the number (1-100) program picked up"<<std::endl;
 int secretNumber;
@@ -24,9 +26,17 @@ do
     std::cout<<"I'm ready, Guess the secret number"<<std::endl;
     secretNumber = pickRandomNumber();
     chances = 7;
+
     while(chances!=0)
     {
-        std::cin>>answer;
+       do
+        {
+            std::cin>>answer;
+            succes=std::cin.fail();
+            std::cin.clear();
+            std::cin.ignore( std::numeric_limits < std::streamsize >::max(), '\n');
+        } while(succes);
+
         if(answer == secretNumber)
         {
             std::cout<<"You won!!!!!\n";
@@ -34,11 +44,15 @@ do
         }
         if(answer != secretNumber)
         {
-            chances--;
-            if(chances==0) break;
+           chances--;
+           if(chances==0)
+           {
+               std::cout<<"You loose"<<" You have "<<chances<< " more chances\n";
+               break;
+           }
            std::cout<<"Try one more time"<<" You have "<<chances<< " more chances\n";
-           if(answer>secretNumber) std::cout<<"Secret number is less than your answer"<<std::endl;
-           if(answer<secretNumber) std::cout<<"Secret number is greater than your answer"<<std::endl;
+           if(answer>secretNumber) std::cout<<"Secret number is LESS than your answer"<<std::endl;
+           if(answer<secretNumber) std::cout<<"Secret number is GREATER than your answer"<<std::endl;
 
         }
     }
