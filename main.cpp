689734728537e170f9859ed7c801bf34311404be
MarcinML <marcinliszcz@gmail.com>
2018-03-13 12:18:44 +0100

    Game is working

--- main.cpp
+++ main.cpp
@@ -2,8 +2,53 @@
 #include <ctime>
 #include <cstdlib>
 
+int pickRandomNumber()
+{
+    int random =( std::rand() % 101 ) + 1;
+    return random;
+}
+
 int main()
 {
+srand(time(NULL));
+char quit=0;
+int chances;
+int answer=0;
+
+std::cout<<"Welcome to Higher/Lower game!\nRules are simple:\n-You have 7 chances to guess the number (1-100) program picked up"<<std::endl;
+int secretNumber;
+
+
+do
+{
+    std::cout<<"I'm ready, Guess the secret number"<<std::endl;
+    secretNumber = pickRandomNumber();
+    chances = 7;
+    while(chances!=0)
+    {
+        std::cin>>answer;
+        if(answer == secretNumber)
+        {
+            std::cout<<"You won!!!!!\n";
+            break;
+        }
+        if(answer != secretNumber)
+        {
+            chances--;
+            if(chances==0) break;
+           std::cout<<"Try one more time"<<" You have "<<chances<< " more chances\n";
+           if(answer>secretNumber) std::cout<<"Secret number is less than your answer"<<std::endl;
+           if(answer<secretNumber) std::cout<<"Secret number is greater than your answer"<<std::endl;
+
+        }
+    }
+
+    std::cout<<"Secret number was "<< secretNumber << "\nWanna play again? (y/n): "<<std::endl;
+    std::cin>>quit;
+
+}while(quit == 'y' || quit == 'Y');
+
+
 
 
     return 0;
